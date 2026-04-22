e8b5e6969af0a4552dee4fc43c340bfa2943e196
Jumaevargen <newargen03@gmail.com>
2026-04-22 22:17:58 +0600

    FINISH PROJECT

--- js/basket.js
+++ js/basket.js
@@ -1,4 +1,4 @@
-const BASKET = JSON.parse(localStorage.getItem('basket')) || [];
+let BASKET = JSON.parse(localStorage.getItem('basket')) || [];
 const BASKET_CARDS = document.querySelector('.basket');
 
 function RENDER_BASKET_PRODUCTS(data) {
@@ -7,4 +7,39 @@ function RENDER_BASKET_PRODUCTS(data) {
   if (data.length === 0) {
     return 'Ваше корзина пустая';
   }
+
+  BASKET_CARDS.innerHTML = '';
+  data.forEach((b) => {
+    const div = document.createElement('div');
+    div.className = 'basket-cards';
+    const image = document.createElement('img');
+    image.src = b.image;
+    const name = document.createElement('h3');
+    name.textContent = b.name;
+    const count = document.createElement('p');
+    count.textContent = 'Count: ' + b.count;
+    const btn = document.createElement('button');
+    btn.textContent = 'Delete';
+    btn.dataset.id = b.id;
+
+    div.appendChild(image);
+    div.appendChild(name);
+    div.appendChild(count);
+    div.appendChild(btn);
+    BASKET_CARDS.appendChild(div);
+  });
 }
+BASKET_CARDS.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const id = e.target.dataset.id;
+    DELETE_ITEM(id);
+  }
+});
+function DELETE_ITEM(id) {
+  const ITEM_ID = Number(id);
+  BASKET = BASKET.filter((el) => el.id !== ITEM_ID);
+  localStorage.setItem;
+  ('basket', JSON.stringify(BASKET));
+  RENDER_BASKET_PRODUCTS(BASKET);
+}
+RENDER_BASKET_PRODUCTS(BASKET);
